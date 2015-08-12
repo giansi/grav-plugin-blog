bc50c1797ace9fe5a25383cf5355bd09edb93803
Giansimon <giansimon.diblas@gmail.com>
2015-08-12 13:13:45 +0200

    -

---
+++ blog.php
@@ -0,0 +1,46 @@
+<?php
+namespace Grav\Plugin;
+
+use \Grav\Common\Plugin;
+
+class BlogPlugin extends Plugin
+{
+    /**
+     * @return array
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            'onPluginsInitialized' => ['onPluginsInitialized', 0]
+        ];
+    }
+
+    /**
+     * Initialize configuration
+     */
+    public function onPluginsInitialized()
+    {
+        $this->enable([
+            'onTwigTemplatePaths' => ['onTwigTemplatePaths', 0],
+            'onTwigSiteVariables' => ['onTwigSiteVariables', 0]
+        ]);
+    }
+
+    /**
+     * Add current directory to twig lookup paths.
+     */
+    public function onTwigTemplatePaths()
+    {
+        $this->grav['twig']->twig_paths[] = __DIR__ . '/templates';
+    }
+
+    /**
+     * Set needed variables to display breadcrumbs.
+     */
+    public function onTwigSiteVariables()
+    {
+        $this->grav['assets']->add('plugin://blog/css-compiled/template.css');
+	        
+        $this->grav['twig']->twig_vars['blog_sidebar'] = $this->config->get('plugins.blog.sidebar');
+    }
+}
